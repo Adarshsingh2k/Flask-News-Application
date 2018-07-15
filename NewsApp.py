aa31ddc1f68ac7a45436e11cd57390bc25c0acea
Gunnika <b.gunnika@gmail.com>
2018-07-15 17:07:10 +0530

    Login Authentication

--- NewsApp.py
+++ NewsApp.py
@@ -1,16 +1,50 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, session, flash, redirect,abort 
 from newsapi.articles import Articles
 from newsapi.sources import Sources
-import requests
+#from flask.ext.socketio import SocketIO, emit
+from sqlalchemy.orm import sessionmaker
+import os
+from tabledef import *
+engine = create_engine('sqlite:///database.db', echo=True)
 
 app = Flask(__name__)
 a = Articles(API_KEY="867af1dffb80450b9770b4bcc10c8e14")
 s = Sources(API_KEY="867af1dffb80450b9770b4bcc10c8e14")
+"""app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
+@socketio.on('my event')                          # Decorator to catch an event called "my event":
+def test_message(message):                        # test_message() is the event callback function.
+    emit('my response', {'data': 'got it!'})      # Trigger a new event called "my response" 
+ """                                                 # that can be caught by another callback later in the program.
 
-@app.route("/<user>")
-def homepage(user):
-	return render_template("Welcome.html", name=user)
+@app.route("/")
+def home():
+	if session.get('logged_in'):
+		return render_template("Welcome.html")
+	else: 
+		return render_template("login.html")
+
+@app.route('/login',methods=["POST"])
+def do_admin_login():
+ 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
 
 @app.route("/headlines")
 def headlines():
@@ -70,4 +104,5 @@ def gaming():
 
 
 if __name__=='__main__':
+	app.secret_key = os.urandom(12)
 	app.run(host='0.0.0.0', debug=True)
